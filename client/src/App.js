0750b89b9724335468f644f317d5b0ac97842b8e
GiangiMillone00 <gianlucamillone2000@gmail.com>
2025-03-27 12:20:23 +0100

    Client Side

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
+import VoceLLM from "./VoceLLM";
+import FormNewVoce from "./FormNewVoce";
 
 function App() {
+  const [elencoLLM, setElencoLLM] = useState(null);
+
+  const reloadData = useEffect(() => {
+    fetch("http://localhost:8000/llms")
+      .then((response) => response.json())
+      .then((data) => {
+        setElencoLLM(data.llms);
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    var queries = [];
+    if (document.getElementById("txtCategory").value !== "") {
+      queries.push(
+        document.getElementById("txtCategory").name +
+          "=" +
+          document.getElementById("txtCategory").value
+      );
+    }
+    if (document.getElementById("txtCompany").value !== "") {
+      queries.push(
+        document.getElementById("txtCompany").name +
+          "=" +
+          document.getElementById("txtCompany").value
+      );
+    }
+
+    var q_str = "";
+    if (queries !== "") q_str = "?" + queries.join("&");
+
+    console.log(q_str);
+
+    fetch("http://localhost:8000/llms" + q_str)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setElencoLLM(data.llms);
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  };
+
+  if (elencoLLM == null) {
+    return (
+      <div className="App">
+        <h2>Elenco LLMs</h2>
+        <div>Nessun LLMs</div>
+      </div>
+    );
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="leftElenco">
+        <h2>Elenco LLMs</h2>
+        <table className="tableLLM">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>company</th>
+              <th>category</th>
+              <th>release_date</th>
+              <th>model_name</th>
+              <th>num_million_parameters</th>
+            </tr>
+          </thead>
+          <tbody>
+            {elencoLLM.map((element) => {
+              return <VoceLLM key={element.id} el={element}></VoceLLM>;
+            })}
+          </tbody>
+        </table>
+
+        <form onSubmit={handleSubmit}>
+          Filtri:
+          <br />
+          company:<input id="txtCompany" name="fCompany"></input>
+          category:<input id="txtCategory" name="fCategory"></input>
+          <br />
+          <button id="btnFiltra" type="submit">
+            Filtra
+          </button>
+        </form>
+      </div>
+      <div className="rightInput">
+        <FormNewVoce reloadData={reloadData}></FormNewVoce>
+      </div>
     </div>
   );
 }
--- server/main.py
+++ server/main.py
@@ -9,8 +9,18 @@ from motor.motor_asyncio import (
 from pydantic import BaseModel
 import csv, os
 from typing import Optional
+from fastapi.middleware.cors import CORSMiddleware
+from bson.objectid import ObjectId
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000", "http://localhost:3001"],  # Only allow requests from React app
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
+
 llms_collection: AsyncIOMotorCollection = AsyncIOMotorClient(
     "mongodb://root:example@mongodb:27017"
 )["plino"]["llms"]
@@ -22,21 +32,24 @@ FILE_DIR = os.path.join(BASE_DIR, "..", "data")  # Move up and locate the 'data'
 # Resolve any symbolic links or relative paths
 FILE_DIR = os.path.realpath(FILE_DIR)
 
-
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
 
 class LLMObj(BaseModel):
+    id: str
     company: str
     category: str
     release_date: str
     model_name: str
     num_million_parameters: str
 
+    class Config:
+        orm_mode = True
+
 @app.post("/llm", status_code=status.HTTP_201_CREATED, response_model=None)
 async def create_llm(
-    el: Optional[LLMObj] = None):
+    el: LLMObj = None):
 
     if el:
         print("--> NOT NULL")
@@ -56,7 +69,7 @@ async def create_llm(
             spamreader = list(reader)
             headerreader = spamreader[0]
             chosen_row = random.choice(spamreader[1:])
-            el = LLMObj(company=chosen_row[0], category=chosen_row[1], release_date=chosen_row[2], model_name=chosen_row[3], num_million_parameters=chosen_row[4])
+            el = LLMObj(id=0,company=chosen_row[0], category=chosen_row[1], release_date=chosen_row[2], model_name=chosen_row[3], num_million_parameters=chosen_row[4])
 
     search_el = await llms_collection.find_one(
         {"company": el.company, "category": el.category, "release_date": el.release_date, "model_name": el.model_name, "num_million_parameters": el.num_million_parameters}
@@ -76,11 +89,19 @@ class GetLLMsResponseBody(BaseModel):
     status_code=status.HTTP_200_OK,
     response_model=GetLLMsResponseBody,
 )
-async def get_llms() -> GetLLMsResponseBody:
+async def get_llms(fCompany: str = None, fCategory: str = None) -> GetLLMsResponseBody:
     
     elems = []
 
-    async for el in llms_collection.find():
-        elems.append(LLMObj(company=el["company"], category=el["category"], release_date=el["release_date"], model_name=el["model_name"], num_million_parameters=el["num_million_parameters"]))
+    filter = {}
+    if fCompany:
+        filter["company"] = {"$regex": fCompany}
+
+    if fCategory:
+        filter["category"] = {"$regex": fCategory}
+
+    async for el in llms_collection.find(filter):
+        el["id"] = str(el["_id"])
+        elems.append(LLMObj(id=el["id"], company=el["company"], category=el["category"], release_date=el["release_date"], model_name=el["model_name"], num_million_parameters=el["num_million_parameters"]))
 
     return {'llms': elems}
